a6f6641a2bfeff71976090ac7fdff89a5a1b3ae8
TAKE1354 <k017c1354@it-neec.jp>
2019-06-20 09:38:59 +0900

    fucking code

---
+++ src/components/Dairypost.vue
@@ -0,0 +1,37 @@
+<template>
+    <div>
+    <input type='text' v-model="sendDiary.title" placeholder="title">
+    <input type='text' v-model="sendDiary.content" placeholder="content">
+    <button v-on:click="send">send</button>        
+    </div>
+</template>
+
+<script>
+//import axios from 'axios'
+const axios = require('axios');
+
+export default {
+  name: 'Dairypost',
+data() {
+
+    return {
+      //dataに直にkeyvalueをつくっておくれるか試してみる
+      //sendDiary: this.sendDiary的なことをaxiosで書く
+      diarys: [
+      ],
+      sendDiary: {
+        title: "",
+        content: ""
+      }
+    };
+  },
+  methods: {
+  send() {
+      axios.post('http://tatikaze.com/post',{title: this.sendDiary.title, content: this.sendDiary.content})
+        .then(function(response) {
+        console.log(response.data.diarys);
+      })
+    }
+  }
+}
+</script>
